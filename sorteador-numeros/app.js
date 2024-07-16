83d2c0da12056cea9f0e668aee0ef431bdb7eee6
mhviana1 <mhviana.adm@gmail.com>
2024-07-15 22:42:42 -0300

    incluindo botao sorteador

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+        alert(`Quantidade é: ${quantidade}`);
+        alert(`De: ${de}`)
+        alert(`Até: ${ate}`)
+}
