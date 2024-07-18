242b473ce5e68f79414a90c2afb6141452400b05
mhviana1 <mhviana.adm@gmail.com>
2024-07-18 11:39:19 -0300

    18/07 11h40

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,4 +1,18 @@
 function alterarStatus(id) {
-    let gameCLicado =  document.getElementById(`game- ${id}`)
-    let imagem = gameCLicado.querySelector('')
+    let gameClicado =  document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button ');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')){
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        
+    } else {
+    imagem.classList.add('dashboard__item__img--rented')
+    botao.classList.add('dashboard__item__button--return')
+    botao.textContent = 'Devolver'
+    }
+    
+
 }
